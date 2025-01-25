f9c4e2f0db8617c0acd358e1dade1ae9d341bfe1
hang <hang14756@163.com>
2025-01-25 23:48:35 +0800

    向量库的添加

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello")
+import weaviate
+from weaviate.auth import AuthApiKey
+
+# 本地部署的
+# client = weaviate.connect_to_local(
+#     auth_credentials=AuthApiKey("test-secret-key")
+# )
+
+client = weaviate.connect_to_custom(
+    skip_init_checks=False,
+    http_host="172.20.8.178",
+    http_port=8080,
+    http_secure=False,
+    grpc_host="172.20.8.178",
+    grpc_port=50051,
+    grpc_secure=False,
+    # 对应AUTHENTICATION_APIKEY_ALLOWED_KEYS中的密钥
+    # 注意：此处只需要密钥即可，不需要用户名称
+    auth_credentials=AuthApiKey("test-secret-key")
+)
+print(client.is_ready())
+print(client.close())
